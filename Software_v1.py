dca7bec5fb9933da10a86a009f78cb1d1ec87a3d
Antônio Marcos <77894621+anttonimarco@users.noreply.github.com>
2021-01-25 04:15:59 -0300

    Building in process ^^

--- Software_v1.py
+++ Software_v1.py
@@ -1,7 +1,6 @@
 # OBS> Seguir regra PEP 8
 from tkinter import *  # Importação da biblioteca
 
-
 menu = Tk()  # Declaração da variável para receber o TK
 
 
@@ -14,11 +13,9 @@ class menu_cadastro:  # Criação da classe.
         self.botoes()
         self.menu.mainloop()
 
-
-
     def menucadastro(self):  # Configuração do menu
         self.menu.title('Cadastro de Clientes')
-        self.menu.geometry('800x900+600+150')
+        self.menu.geometry('800x800+600+150')
         self.menu.resizable(True, True)
         self.menu.maxsize(height=800, width=800)
         self.menu.minsize(height=600, width=600)
@@ -26,7 +23,7 @@ class menu_cadastro:  # Criação da classe.
 
         self.quadrado1 = Frame(self.menu, background='#00CCFF', highlightbackground='#3399FF', highlightthickness=3,
                                bd=5)  # Criação de um Frame (área retangular)
-        self.quadrado1.place(relx=0.04, rely=0.05, relheight=0.55, relwidth=0.92)
+        self.quadrado1.place(relx=0.04, rely=0.05, relheight=0.89, relwidth=0.92)
 
         self.testimagem = Frame(self.menu, background='yellow')
         self.testimagem.place(relx=0.09, rely=0.10, relheight=0.20, relwidth=0.15)
@@ -80,6 +77,14 @@ class menu_cadastro:  # Criação da classe.
         self.txt_telefone = Label(self.menu, text='Telefone', background='#00CCFF')
         self.txt_telefone.place(relx=0.08, rely=0.50, relheight=0.05, relwidth=0.10)
 
+        self.txt_celular = Label(self.menu, text='Celular', background='#00CCFF')
+        self.txt_celular.place(relx=0.35, rely=0.50, relheight=0.05, relwidth=0.10)
+
+        self.txt_email = Label(self.menu, text='E-Mail', background='#00CCFF')
+        self.txt_email.place(relx=0.63, rely=0.50, relheight=0.05, relwidth=0.10)
+
+        self.txt_observacao = Label(self.menu, text='Observações', background='#00CCFF')
+        self.txt_observacao.place(relx=0.09, rely=0.58, relheight=0.05, relwidth=0.10)
 
     def entradadados(self):  # Criação dos campos da entrada de dados.
         self.entrada_codigo = Entry(self.menu)
@@ -115,7 +120,7 @@ class menu_cadastro:  # Criação da classe.
         self.entrada_numero.place(relx=0.71, rely=0.33, relheight=0.03, relwidth=0.20)
 
         self.entrada_complemento = Entry(self.menu)
-        self.entrada_complemento.place(relx=0.27, rely=0.40, relheight=0.03, relwidth=0.30)
+        self.entrada_complemento.place(relx=0.27, rely=0.40, relheight=0.03, relwidth=0.31)
 
         self.entrada_bairro = Entry(self.menu)
         self.entrada_bairro.place(relx=0.61, rely=0.40, relheight=0.03, relwidth=0.30)
@@ -130,19 +135,33 @@ class menu_cadastro:  # Criação da classe.
         self.entrada_cidade.place(relx=0.65, rely=0.47, relheight=0.03, relwidth=0.26)
 
         self.entrada_telefone = Entry(self.menu)
-        self.entrada_telefone.place(relx=0.09, rely=0.54, relheight=0.03, relwidth=0.26)
+        self.entrada_telefone.place(relx=0.09, rely=0.54, relheight=0.03, relwidth=0.25)
 
+        self.entrada_celular = Entry(self.menu)
+        self.entrada_celular.place(relx=0.37, rely=0.54, relheight=0.03, relwidth=0.25)
+
+        self.entrada_email = Entry(self.menu)
+        self.entrada_email.place(relx=0.65, rely=0.54, relheight=0.03, relwidth=0.26)
+
+        self.entrada_observacao = Entry(self.menu)
+        self.entrada_observacao.place(relx=0.09, rely=0.62, relheight=0.10, relwidth=0.82)
 
     # Criação dos botões.
     def botoes(self):
         self.select_image = Button(self.menu, text='Carregar Foto')
         self.select_image.place(relx=0.09, rely=0.33, relheight=0.04, relwidth=0.15)
 
-        self.select_limpar = Button(self.menu, text='Limpar')
-        self.select_limpar.place(relx=0.09, rely=0.39, relheight=0.04, relwidth=0.15)
+        self.botao_limpar = Button(self.menu, text='Limpar')
+        self.botao_limpar.place(relx=0.09, rely=0.39, relheight=0.04, relwidth=0.15)
 
+        self.botao_cadastrar = Button(self.menu, text='Cadastrar')
+        self.botao_cadastrar.place(relx=0.15, rely=0.77, relheight=0.07, relwidth=0.15)
 
+        self.botao_cadastrar = Button(self.menu, text='Banco de Dados')
+        self.botao_cadastrar.place(relx=0.42, rely=0.77, relheight=0.07, relwidth=0.15)
 
+        self.botao_cadastrar = Button(self.menu, text='Pesquisar e Alterar')
+        self.botao_cadastrar.place(relx=0.70, rely=0.77, relheight=0.07, relwidth=0.15)
 
 
 menu_cadastro()
